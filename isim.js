3f816d33e1fcd58fa4013972abe63783a233d810
Enes Sapan <115135140+Feel02@users.noreply.github.com>
2023-04-26 18:38:58 +0300

    Add files via upload

--- isim.js
+++ isim.js
@@ -1,14 +1,27 @@
-const myForm = document.getElementById("myForm");
+const myForm = document.getElementById("myForm");     //get .csv file
 const csvFile = document.getElementById("csvFile");
 
-myForm.addEventListener("submit", function (e) {
+const reader = new FileReader();                      //create a filereader
+
+myForm.addEventListener("submit", function (e) {      //use the submit button
   e.preventDefault();
   const input = csvFile.files[0];
+
+  reader.onload = function (e) {                      //gets every row individually from file
+    const text = e.target.result;
+    const data = csvToArray(text);                    //calls the array for parsing
+    document.write(JSON.stringify(data));             //and prints for debug(will be changed)
+  };
+  
+  reader.readAsText(input);                           //read a new line
 });
 
-const reader = new FileReader();
-reader.onload = function (event) {
-  console.log(event.target.result); // the CSV content as string
-};
-
-reader.readAsText(file);
+function csvToArray(str, delimiter = ";") {         //parse the text into array function
+    
+  let array = str.split("\r\n").map(function (line) {
+    return line.split(delimiter);
+  });
+    array.pop();                                    //last item of array is [""] so we popped it
+    console.log(array.length);                      //for debug (will be removed)
+    return array;
+  }
